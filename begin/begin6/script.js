e5017dd7a7b21b38026dfba83ba6dc292bfb8703
hokim <hokim.khujayorzoda@gmail.com>
2021-09-06 19:16:23 +0500

    Begin 6

--- begin/begin6/script.js
+++ begin/begin6/script.js
@@ -3,8 +3,9 @@ const b = 5;
 const c = 6;
 const V = (parseInt(a) * parseInt(b) * parseInt(c));
 
-document.write("V = ",(parseInt(a) * parseInt(b) * parseInt(c)));
+document.write( "V = ",(parseInt(a) * parseInt(b) * parseInt(c)));
 
 const S = 2 * (parseInt(a) * parseInt(b) + parseInt(b) * parseInt(c) + parseInt(a) * parseInt(c));
 document.write('\n');
-document.write("S = ",2 * (parseInt(a) * parseInt(b) + parseInt(b) * parseInt(c) + parseInt(a) * parseInt(c)));
+
+document.write( "S = ",2 * (parseInt(a) * parseInt(b) + parseInt(b) * parseInt(c) + parseInt(a) * parseInt(c)));
