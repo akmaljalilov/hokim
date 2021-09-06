1697577a7442737cf7e3d4aa2bf09fe54b67aa10
hokim <hokim.khujayorzoda@gmail.com>
2021-09-06 19:13:24 +0500

    Begin 5

--- begin/begin5/script.js
+++ begin/begin5/script.js
@@ -1,3 +1,6 @@
 const a  = 2;
 const V = (parseInt(a) * parseInt(a) * parseInt(a));
-document.write(V);
+const S = 6 * (parseInt(a) * parseInt(a));
+document.write( "V = ", (parseInt(a) * parseInt(a) * parseInt(a)),);
+document.write('\n');
+document.write( "S = ",6 * (parseInt(a) * parseInt(a)));
---
+++ begin/begin6/script.js
@@ -0,0 +1,10 @@
+const a = 4;
+const b = 5;
+const c = 6;
+const V = (parseInt(a) * parseInt(b) * parseInt(c));
+
+document.write("V = ",(parseInt(a) * parseInt(b) * parseInt(c)));
+
+const S = 2 * (parseInt(a) * parseInt(b) + parseInt(b) * parseInt(c) + parseInt(a) * parseInt(c));
+document.write('\n');
+document.write("S = ",2 * (parseInt(a) * parseInt(b) + parseInt(b) * parseInt(c) + parseInt(a) * parseInt(c)));
