fd954b50818e641766d1127525180385cce6050d
hokim <hokim.khujayorzoda@gmail.com>
2021-09-06 19:42:07 +0500

    Begin 7

---
+++ begin/begin7/script.js
@@ -0,0 +1,6 @@
+const π = 3.14;
+const R = 5;
+const L = 2 * (parseInt(π) * parseInt(R));
+document.write("L = ", 2 * (parseInt(π) * parseInt(R)));
+document.write('\n');
+document.write("S = ", (parseInt(π) * parseInt(R) * parseInt(R)));
