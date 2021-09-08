5d39ab5c07f5107c34dfd973a3b208bf80a12a82
hokim <hokim.khujayorzoda@gmail.com>
2021-09-08 18:57:29 +0500

    Begin 13

---
+++ begin/begin13/script.js
@@ -0,0 +1,14 @@
+const R1 = 6;
+const R2 = 4;
+const π = 3.14;
+const S1 = π * Math.pow(R1,2);
+document.write("S1 = ",S1);
+
+document.write('\n');
+
+const S2 = π * Math.pow(R2,2);
+document.write("S2 = ",S2);
+
+document.write('\n');
+const S3 = S1 - S2;
+document.write("S3 = ",S3);
