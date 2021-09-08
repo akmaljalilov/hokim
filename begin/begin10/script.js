c0c8bc41bf9f616ea11c22fd8c169e7fb8737bf0
hokim <hokim.khujayorzoda@gmail.com>
2021-09-08 14:10:14 +0500

    Begin 10

---
+++ begin/begin10/script.js
@@ -0,0 +1,17 @@
+const a = 5;
+const b = 6;
+const S = a * b;
+const s = a + b;
+document.write("S = ",parseInt(a) * parseInt(b));
+document.write('\n');
+document.write("s = ",parseInt(a) + parseInt(b));
+
+document.write('\n');
+
+const c = 4;
+const d = 2;
+const p = a - b;
+document.write("P = ",parseInt(c) - parseInt(d));
+document.write('\n');
+document.write("p = ",parseInt(c) / parseInt(d));
+
---
+++ begin/begin9/script.js
@@ -0,0 +1,4 @@
+const a = 5;
+const b = 5;
+const S = parseInt(a) * parseInt(b);
+document.write("S = ",parseInt(a) * parseInt(b));
