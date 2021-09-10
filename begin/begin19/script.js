19ccad91171515ee64168ee766fae7b3113714ca
hokim <hokim.khujayorzoda@gmail.com>
2021-09-10 14:24:34 +0500

    Begin 19

---
+++ begin/begin19/script.js
@@ -0,0 +1,9 @@
+const x1 = 9;
+const y1 = 8;
+const x2 = 7;
+const y2 = 4;
+const P = 2 * (x1 - x2) + (y1 - y2);
+document.write("P = ",P);
+document.write('\n');
+const S = (x1 - x2) * (y1 - y2);
+document.write("S = ",S);
