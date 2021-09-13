ec56a18df30d159cc0cbdfae9e437043a1a86b8b
hokim <hokim.khujayorzoda@gmail.com>
2021-09-13 20:14:46 +0500

    Begin 28

---
+++ begin/begin28/script.js
@@ -0,0 +1,8 @@
+let A = 2;
+let A1 = Math.pow(A,2);
+let A2 = Math.pow(A,3);
+let A3 = Math.pow(A,5);
+let A4 = Math.pow(A,10);
+let A5 = Math.pow(A,15);
+let A6 = A1*A2*A3*A4*A5;
+document.write("A6= ",A6);
