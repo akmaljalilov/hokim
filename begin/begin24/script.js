f8945e34a2e1c04e28d238e5486eca232557a2ed
hokim <hokim.khujayorzoda@gmail.com>
2021-09-13 17:13:34 +0500

    Begin 24

---
+++ begin/begin24/script.js
@@ -0,0 +1,10 @@
+let A = 4;
+let B = 5;
+let C = 6;
+document.writeln("A=",A , " B=",B, "C=",C);
+let S = A;
+A = C;
+C = B;
+B = S;
+document.write(" | ");
+document.writeln("A=",A,"B=",B,"C=",C,);
