f05520e4322fd7c485400f12bbb721e2437d8f3d
hokim <hokim.khujayorzoda@gmail.com>
2021-09-14 16:38:32 +0500

    Begin 39

---
+++ begin/begin35/script.js
@@ -0,0 +1,2 @@
+const V = 6;
+const U = 4;
---
+++ begin/begin39/script.js
@@ -0,0 +1,11 @@
+const A = 1;
+const B = 14;
+const C =45;
+const D = Math.pow(B,2) - 4*A*C;
+document.write("D= ",D);
+document.write('\n');
+const X1 = (-B+Math.sqrt(D))/2*A;
+document.write("X1= ",X1);
+document.write('\n');
+const X2 = (-B-Math.sqrt(D))/2*A;
+document.write("X2= ",X2)
